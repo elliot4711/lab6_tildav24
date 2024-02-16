b10ee0ed95e51d16b6903278a41df5eeb271754a
Elliot Stjernqvist <elliot@n143-p15.eduroam.kth.se>
2024-02-16 13:53:46 +0100

    Added search functions

--- search_test.py
+++ search_test.py
@@ -1,3 +1,4 @@
+import random
 class songinfo:
     
     def __init__(self, trackid, songid, artistname, songname):
@@ -9,19 +10,86 @@ class songinfo:
     def getartistname(self):
         return self.artistname
     
-    def getsongname(self):
+    def getsongname(self):  
         return self.songname
 
     def __lt__(self, other):
         return self.artistname < other.artistname
 
-infolist = []
+def read_list():
+    infolist = []
+    with open("unique_tracks.txt", "r", encoding = "utf-8") as trackfile:
+        for row in trackfile:
+            row = row.split("<SEP>")
+            row[3] = row[3].strip("\n")
+            infolist.append(songinfo(row[0], row[1], row[2], row[3]))
 
-with open("unique_tracks.txt", "r", encoding = "utf-8") as trackfile:
-    for row in trackfile:
-        row = row.split("<SEP>")
-        row[3] = row[3].strip("\n")
-        infolist.append(songinfo(row[0], row[1], row[2], row[3]))
+    return infolist
 
-print(infolist[-1].getsongname())
-print(infolist[-1].getartistname())
+infolist = read_list()
+
+def linear_search(the_list):
+    """
+    From lecture 4
+    """
+    key_value = random.randint(0, len(the_list) + 1)
+    key = the_list[key_value].getartistname()
+
+    for x in the_list:
+        if x.getartistname() == key:
+            print(x.getartistname())
+            print(key)
+            return True
+    return False
+
+def binary_search(the_list):
+    """
+    Partially from chatgpt
+    """
+
+    key_value = random.randint(0, len(the_list) + 1)
+    key = the_list[key_value].getartistname()
+    
+    low = 0
+    high = len(the_list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if the_list[mid].getartistname() == key:
+            return the_list[mid].getartistname()
+        elif the_list[mid].getartistname() < key:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return None
+
+def sort_list(infolist):
+    """
+    From stackoverflow
+    """
+    infolist.sort(key=lambda x: x.artistname, reverse=False)
+    return infolist
+
+def make_dict(infolist):
+    namedict = dict()
+    for object in infolist:
+        namedict[object.getartistname()] = object
+
+    return namedict
+
+def hashtable_search(namedict, the_list):
+    key_value = random.randint(0, len(the_list) + 1)
+    key = the_list[key_value].getartistname()
+    print(key)
+    print(namedict[key].getartistname())
+    if key in namedict:
+        return namedict[key].getartistname()
+    else: 
+        return None
+
+namedict = make_dict(infolist)
+hashtable_search(namedict, infolist)
+
+
+
