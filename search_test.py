9ed70ef4cdd1473d8e5e9564e0ea8f381bb65be1
Elliot Stjernqvist <elliot@Elliots-Air.localdomain>
2024-02-17 11:56:31 +0100

    Fixed issues

--- search_test.py
+++ search_test.py
@@ -33,11 +33,11 @@ def linear_search(the_list):
     """
     From lecture 4
     """
-    #key_value = random.randint(0, len(the_list))
-    key = the_list[len(the_list)-1].getartistname()
+    key_value = random.randint(0, len(the_list))
+    key = the_list[key_value].getsongname()
 
     for x in the_list:
-        if x.getartistname() == key:
+        if x.getsongname() == key:
             return True
     return False
 
@@ -47,16 +47,16 @@ def binary_search(the_list):
     """
 
     key_value = random.randint(0, len(the_list))
-    key = the_list[key_value].getartistname()
+    key = the_list[key_value].getsongname()
     
     low = 0
     high = len(the_list) - 1
 
     while low <= high:
         mid = (low + high) // 2
-        if the_list[mid].getartistname() == key:
-            return the_list[mid].getartistname()
-        elif the_list[mid].getartistname() < key:
+        if the_list[mid].getsongname() == key:
+            return the_list[mid].getsongname()
+        elif the_list[mid].getsongname() < key:
             low = mid + 1
         else:
             high = mid - 1
@@ -67,37 +67,40 @@ def sort_list(infolist):
     """
     From stackoverflow
     """
-    infolist.sort(key=lambda x: x.artistname, reverse=False)
+    infolist.sort(key=lambda x: x.songname, reverse=False)
     return infolist
 
 def make_dict(infolist):
     namedict = dict()
     for object in infolist:
-        namedict[object.getartistname()] = object
+        namedict[object.getsongname()] = object
 
     return namedict
 
 def hashtable_search(namedict, the_list):
     key_value = random.randint(0, len(the_list))
-    key = the_list[key_value].getartistname()
+    key = the_list[key_value].getsongname()
     if key in namedict:
-        return namedict[key].getartistname()
+        return namedict[key].getsongname()
     else: 
         return None
 
 numbers_tried = 10000
 infolist = read_list()
+oldlist = infolist
+infolist = infolist[0:250000]
 
 linjtime = timeit.timeit(stmt = lambda: linear_search(infolist), number = numbers_tried)
-print("Linearsearch took", round(linjtime, 8) , "seconds")
+print("Linearsearch took", round(linjtime / numbers_tried, 8) , "seconds")
 
 infolist = sort_list(infolist)
 bintime = timeit.timeit(stmt = lambda: binary_search(infolist), number = numbers_tried)
-print("Binarysearch took", round(bintime, 8) , "seconds")
+print("Binarysearch took", round(bintime / numbers_tried, 8) , "seconds")
 
-namedict = make_dict(infolist)
-hashtime = timeit.timeit(stmt = lambda: hashtable_search(namedict, infolist), number = numbers_tried)
-print("Hashsearch took", round(hashtime, 8) , "seconds")
+namedict = make_dict(oldlist)
+hashtime = timeit.timeit(stmt = lambda: hashtable_search(namedict, oldlist), number = numbers_tried)
+print("Hashsearch took", round(hashtime / numbers_tried, 8) , "seconds")
+
 
 
 
